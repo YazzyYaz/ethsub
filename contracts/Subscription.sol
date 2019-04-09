5761b568a1cd7f4d23bed24dd2b5b1304a4694b9
Anmol Singh <anmoldeepsingh365@gmail.com>
2019-04-09 10:36:58 +1000

    contracts init

---
+++ contracts/Subscription.sol
@@ -0,0 +1,81 @@
+pragma solidity ^0.5.0;
+
+interface ERC20 {
+    function totalSupply() external view returns (uint supply);
+    function balanceOf(address _owner) external view returns (uint balance);
+    function transfer(address _to, uint _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint _value) external returns (bool success);
+    function approve(address _spender, uint _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint remaining);
+    function decimals() external view returns(uint digits);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
+
+interface User {
+    function pullToken(ERC20 token, uint amount) external returns (bool);
+    function pullEther(uint amount) external  returns (bool);
+}
+
+contract Subscription {
+
+    //plan for failed payments if balance is below they are removes from subscribers or another mapping of address and bool calle lapsedSub
+
+    address owner;
+    address[] public subscribers;
+    mapping (address => bool) public isSubscriber;
+    //some base time
+    //how many days valid
+
+    modifier onlyOwner {
+      require(msg.sender == owner);
+      _;
+    }
+
+    constructor(address _owner) public {
+        owner = _owner;
+    }
+
+     /// @notice Fallback function - recieves ETH but doesn't alter contributor stakes or raised balance.
+    function() external payable {
+    }
+
+    function subscribe(address _sub) public {
+        require(!isSubscriber[_sub]); //only new contributor
+        require(_sub != owner);
+        isSubscriber[_sub] = true;
+        subscribers.push(_sub);
+
+        //if now > base take payment now? or pro rata payment for remaining time
+    }
+
+    function unsubscribe(address _sub) public {
+        require(isSubscriber[_sub]);
+        isSubscriber[_sub] = false;
+        for (uint i=0; i < subscribers.length - 1; i++)
+            if (subscribers[i] == _sub) {
+                subscribers[i] = subscribers[subscribers.length - 1];
+                break;
+            }
+        subscribers.length -= 1;
+
+        //pro rata payment for used time?
+    }
+
+    function claim() public onlyOwner {
+        for (uint i = 0; i < subscribers.length; i++) {
+            uint bal = address(subscribers[i]).balance;
+
+            if (bal >= 5) {
+                require(User(subscribers[i]).pullEther(5));
+            }
+
+            //if balance below remove from subscribers or lapsedSub bool set to true
+        }
+    }
+
+    ///@dev function to check balance only returns balances in opperating and liquidating periods
+    function checkEthBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+}
---
+++ contracts/UserAccount.sol
@@ -0,0 +1,76 @@
+pragma solidity ^0.5.0;
+
+/// @title Fund Wallet - Fund raising and distribution wallet according to stake and incentive scheme.
+/// @dev Not fully tested, use only in test environment.
+
+
+interface ERC20 {
+    function totalSupply() external view returns (uint supply);
+    function balanceOf(address _owner) external view returns (uint balance);
+    function transfer(address _to, uint _value) external returns (bool success);
+    function transferFrom(address _from, address _to, uint _value) external returns (bool success);
+    function approve(address _spender, uint _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint remaining);
+    function decimals() external view returns(uint digits);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
+
+contract UserAccount {
+
+    address payable owner;
+    address payable payee;
+
+    modifier onlyOwner {
+      require(msg.sender == owner);
+      _;
+    }
+
+    modifier onlyPayee {
+      require(msg.sender == payee);
+      _;
+    }
+
+    constructor(address payable _owner) public {
+        owner = _owner;
+    }
+
+    function setPayee(address payable _payee) public onlyOwner {
+        payee = _payee;
+    }
+
+     /// @notice Fallback function - recieves ETH but doesn't alter contributor stakes or raised balance.
+    function() external onlyOwner payable {
+
+    }
+
+    ///@dev function to check balance only returns balances in opperating and liquidating periods
+    function checkEthBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function checkTokBalance(ERC20 token) public view returns (uint) {
+        return token.balanceOf(address(this));
+    }
+
+    /// @dev send erc20token to the reserve address
+    /// @param token ERC20 The address of the token contract
+    function pullToken(ERC20 token, uint amount) external onlyPayee returns (bool){
+        require(token.transfer(payee, amount));
+        return true;
+    }
+
+    ///@dev Send ether to the reserve address
+    function pullEther(uint amount) external onlyPayee returns (bool){
+        address(payee).transfer(amount);
+        return true;
+    }
+    function withdrawToken(ERC20 token, uint amount) public onlyOwner returns (bool){
+        require(token.transfer(owner, amount));
+        return true;
+    }
+    function withdrawEther(uint amount) public onlyOwner returns (bool){
+        owner.transfer(amount);
+        return true;
+    }
+
+}
